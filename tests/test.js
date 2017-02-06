e3c0883cf3981029651e24859204b1aaaa9a77ab
Aleksey Chuyan <a.chuyan@videal.net>
2017-02-06 18:18:13 +0200

    add build method description to the test.js

--- tests/test.js
+++ tests/test.js
@@ -7,50 +7,50 @@ var expect = chai.expect;
 var EmailGenerator = require('../main');
 
 describe('EmailGenerator:', function() {
-    describe('"build" method', function() {
+    describe('build method: generates a list of possible user\'s email address.', function() {
         it('should exist', function() {
             expect(EmailGenerator.build).to.exist;
         });
         it('should have type "function"', function() {
             expect(EmailGenerator.build).to.be.a('function');
         });
-        it('should return an empty array if got no arguments', function() {
+        it('should return an empty array if method got no arguments', function() {
             expect(EmailGenerator.build()).to.be.empty;
         });
         describe('called with companyDomain argument.', function() {
-            it('Result array should contain "firstName_lastName@company.com" email address', function() {
+            it('result array should contain "firstname_lastname@company.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName', undefined, 'company.com')).to.include('firstname_lastname@company.com');
             });
-            it('Result array should contain "info@company.com" email address', function() {
+            it('result array should contain "info@company.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName', undefined, 'company.com')).to.include('info@company.com');
             });
-            it('Result array should contain "support@company.com" email address', function() {
+            it('result array should contain "support@company.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName', undefined, 'company.com')).to.include('support@company.com');
             });
-            it('Result array should contain "office@company.com" email address', function() {
+            it('result array should contain "office@company.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName', undefined, 'company.com')).to.include('office@company.com');
             });
-            it('Result array should not contain "office@gmail.com" email address', function() {
+            it('result array should not contain "office@gmail.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName', undefined, 'company.com')).to.not.include('office@gmail.com');
             });
         });
         describe('called without companyDomain argument.', function() {
-            it('Result array should contain "firstName_lastName@gmail.com" email address', function() {
+            it('result array should contain "firstname_lastname@gmail.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName')).to.include('firstname_lastname@gmail.com');
             });
-            it('Result array should contain "firstname_middlename_lastname@gmail.com" email address', function() {
+            it('result array should contain "firstname_middlename_lastname@gmail.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName', 'middleName')).to.include('firstname_middlename_lastname@gmail.com');
             });
-            it('Result array should contain "firstName_lastName@gmail.com" email address', function() {
+            it('result array should contain "firstname_lastname@gmail.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName')).to.include('firstname_lastname@gmail.com');
             });
-            it('Result array should contain "firstName_lastName@gmail.com" email address', function() {
+            it('result array should contain "firstname_lastname@gmail.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName')).to.include('firstname_lastname@gmail.com');
             });
-            it('Result array should not contain "office@company.com" email address', function() {
+            it('result array should not contain "office@company.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName')).to.not.include('office@company.com');
             });
-            it('Result array should not contain "firstName.lastName@company.com" email address', function() {
+            it('result array should not contain "firstname.lastname@company.com" email address', function() {
                 expect(EmailGenerator.build('firstName', 'lastName')).to.not.include('firstName.lastName@company.com');
             });
         });
